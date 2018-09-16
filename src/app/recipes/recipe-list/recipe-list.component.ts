ab12a5415f23a4a8ea692dc72ad3060beabf3111
Karl San Gabriel <karl.sangabriel@gmail.com>
2018-09-16 15:28:37 +0800

    Signed-off-by: Karl San Gabriel <karl.sangabriel@gmail.com>

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,7 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit 
+{
+  recipes = [];
 
   constructor() { }
 
